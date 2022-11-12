790e4a88ed630cc4ae9ba77cda7162fda787c2f6
nufar87 <57037332+nufar87@users.noreply.github.com>
2022-11-12 22:07:12 +0200

    AppSus delivery 3

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -1,15 +1,17 @@
 export default {
   template: `
        <header class="app-header">
+    
+            <router-link to="/">
             <img class='site-logo' src='../assets/img/AppSus.png' alt="" />
-            <h1>AppSus</h1>
-            </router-link> 
-        
+            </router-link>
+            <router-link to="/"><h1>AppSus</h1></router-link>
+       
+
             <nav>
-                <router-link to="/">Home</router-link> 
                 <router-link to="/keep">Keep</router-link> 
                 <router-link to="/mail">Mail</router-link> 
-                <router-link to="/about">About</router-link>
+                <!-- <router-link to="/about">About</router-link> -->
             </nav>
         </header>
     `,
--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,13 +1,35 @@
 export default {
-	template: `
-        <section class="home-page">
-            <div className="home-text">
-                <h1>Home sweet home</h1>
-                <p>Amazing App</p>
-                <button>Start Now!</button>
-            </div>
-            <img src='../assets/img/home-img.jpg' alt="" />
+  template: `
+        <section class="home-page flex flex-row">
+            <section class="mail-section flex flex-column">
+                <div class="logo flex flex-row">
+                    <img src="https://1000logos.net/wp-content/uploads/2021/05/Gmail-logo.png" alt="" />
+                    <h1>Mail</h1> 
+                </div>
+                <div class="about-app">
+                    <p>
+                    Communicate was never more easy</p>
+                </div>
+                <div>
+                <router-link to="/mail">
+                    <img src="../assets/imgs/mail.png" alt="" />
+                    </router-link> 
+                </div>
+            </section>
+            <section class="keep-section flex flex-column">
+            <div class="logo flex flex-row">
+                    <img src="../assets/imgs/keep.png" alt="" />
+                    <h1>Keep</h1> 
+                </div>
+                <div class="about-app">
+                    <p> Playful way to keep organized</p>
+                </div>
+                <div>
+                    <router-link to="/keep">
+                    <img src="../assets/imgs/keepapp.png" alt="" />
+                    </router-link> 
+                </div>
+            </section>
         </section>
     `,
-}
-// import '../assets/img/home-img.jpg'
+};
