21a50b9cafae39c0ca24883e0abc5fde07e9332f
Ilan Khmelnitsky <ilankhmel@gmail.com>
2022-11-12 19:55:16 +0200

    fix

--- apps/keep/cmps/note-add.cmp.js
+++ apps/keep/cmps/note-add.cmp.js
@@ -15,14 +15,9 @@ export default {
                 <input  
                 v-show="(noteType ==='note-text')" placeholder="Title" @keyup.enter.prevent="addNote(newNote)"  v-model="newNote.info.title"/>
                 <input  
-<<<<<<< HEAD
                 v-show="(noteType!=='note-todos')" :placeholder="PLACE_HOLDERS[noteType] || 'Take a note...' " @keyup.enter.prevent="addNote(newNote)"  v-model="newNote.info.txt"/>
         
                 <todos-edit v-if="noteType==='note-todos'"/>
-=======
-                v-show="(note-type!=='note-todos')" :placeholder="PLACE_HOLDERS[note-type] || 'Take a note...' " @keyup.enter.prevent="addNote(newNote)"  v-model="txt"/>
-                <todosEdit v-if="(note-type==='note-todos')"></todosEdit>
->>>>>>> 821fbbb22fba2d425801b22775f40eb240a9b0e9
                 <div class="btn-setters">
                     <button title="Text" @click="setType('note-text')">
                         <i class="fas fa-font"></i>
@@ -44,12 +39,9 @@ export default {
 
   data() {
     return {
-<<<<<<< HEAD
+
+      
       noteType: '',
-=======
-      txt:"",
-      noteType: 'note-text',
->>>>>>> 821fbbb22fba2d425801b22775f40eb240a9b0e9
       newNote: null,
 
       //   anotherLine: false,
--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -1,21 +1,13 @@
 export default {
   template: `
-<<<<<<< HEAD
-        <header class="app-header flex flex-row justify-between align-center">
-            <router-link to="/">
-=======
-        <header class="app-header">
+       <header class="app-header">
             <img class='site-logo' src='../assets/img/AppSus.png' alt="" />
->>>>>>> 821fbbb22fba2d425801b22775f40eb240a9b0e9
             <h1>AppSus</h1>
-            </router-link> 
-        
-            <nav class="flex flex-row align-center">
-                <router-link to="/keep"> 
-                    <h4>Keep</h4></router-link> 
-                <router-link to="/mail">
-                    <h4>Mail</h4></router-link> 
-                <!-- <router-link to="/about">About</router-link> -->
+            <nav>
+                <router-link to="/">Home</router-link> 
+                <router-link to="/keep">Keep</router-link> 
+                <router-link to="/mail">Mail</router-link> 
+                <router-link to="/about">About</router-link>
             </nav>
         </header>
     `,
